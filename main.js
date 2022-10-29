b954c2c7eadf41e8d8486d19152f074b74b0a7b1
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2022-10-28 21:53:33 -0400

    update

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 
-
-
-
 function change(){
-    let values = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"];
+    const values = [0,1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"]
     let color = "#";
     let hex = document.getElementById("hex")
 
-   for(let i = 0; i < 6; i++){
-        color = color + values[Math.floor(Math.random() * values.length)];
+    for(let i = 0; i < 6; i++){
+        color = color + values[Math.floor(Math.random() * values.length)]
     }
+
     hex.innerText = color;
-    document.body.style.backgroundColor = color
+    document.body.style.backgroundColor = color;
     return color;
 }
